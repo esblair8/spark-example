995cf3ee7eea73355e25eed50851e0e822dd738f
Evan Blair <evan.blair@aquaq.co.uk>
2019-03-26 10:55:27 +0000

    fixed issue with SparkSession app name

--- src/main/scala/starter/SparkSessionWrapper.scala
+++ src/main/scala/starter/SparkSessionWrapper.scala
@@ -8,7 +8,7 @@ object SparkSessionWrapper {
     create a spark conf and set app name to something specific
     which in turn is used for creating a spark context and then an sqlContext (this is used to read hive managed tables)
    */
-  def createSparkSession(appName: String, master: String = master): SparkSession = {
+  def createSparkSession(appName: String): SparkSession = {
     SparkSession.builder
       .appName(appName)
       .enableHiveSupport
